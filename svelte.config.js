a9e1a3c5707dc66b1c050ab5f2229444df445c6a
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2023-08-30 22:05:01 +0200

    Go to svelte next.405 (pre1.0)

--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,7 @@ const config = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: vercel(),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {}
+		adapter: vercel()
 	}
 };
 
