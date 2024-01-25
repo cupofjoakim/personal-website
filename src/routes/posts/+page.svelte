3be404d1298cde0b3e2ba08af2921be32b3adda5
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2024-01-25 15:52:56 +0100

    Add title to posts page

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -11,6 +11,7 @@
 </svelte:head>
 
 <div class="px-4 sm:px-6 py-4 text-base flex flex-col justify-center max-w-screen-md w-full">
+  <h1 class="text-4xl xl:text-5xl font-bold mb-4 xl:mb-8">Posts</h1>
   {#each posts as post}
   <div
         class="p-8 grid gap-2 rounded-3xl shadow-2xl shadow-grey-500/40 sm:shadow-none sm:hover:shadow-2xl transition-shadow duration-500"
